e307e4d009d486195ea153f1905a12dbe0c07950
Beuche <76558020+Beuche@users.noreply.github.com>
2021-07-09 14:30:45 +0200

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def zz():
+    zahl1=0
+    zahl2=0
     while (zahl1 == zahl2):
         zahl1=Math.round(Math.random()*20)
         zahl2=Math.round(Math.random()*20)
@@ -12,7 +14,11 @@ def tauschen(x1,y1):
 
 while True:
     (z1, z2)=zz()
+    a1=z1
+    a2=z2
     if z1>z2:
-        (a1, a2)= tauschen(z1,z2)
-    basic.show_number(a1)
-    basic.show_number(a2)
+        (z1, z2) = tauschen(z1,z2)
+    basic.set_led_color(0xff0000)
+    basic.show_number(z1)
+    basic.set_led_color(0x00ff00)
+    basic.show_number(z2)
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
+let a1: number;
+let a2: number;
 function zz(): number[] {
-    let zahl1: number;
-    let zahl2: number;
+    let zahl1 = 0
+    let zahl2 = 0
     while (zahl1 == zahl2) {
         zahl1 = Math.round(Math.random() * 20)
         zahl2 = Math.round(Math.random() * 20)
@@ -17,10 +19,14 @@ function tauschen(x1: number, y1: number): number[] {
 
 while (true) {
     let [z1, z2] = zz()
+    a1 = z1
+    a2 = z2
     if (z1 > z2) {
-        let [a1, a2] = tauschen(z1, z2)
+        let [z1, z2] = tauschen(z1, z2)
     }
     
-    basic.showNumber(a1)
-    basic.showNumber(a2)
+    basic.setLedColor(0xff0000)
+    basic.showNumber(z1)
+    basic.setLedColor(0x00ff00)
+    basic.showNumber(z2)
 }
